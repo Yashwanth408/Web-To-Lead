085eeee565fdb6e434f796161212d190fe1dc937
Yashwanth408 <Yashwanth408@gmail.com>
2024-03-27 18:23:02 +0530

    added script separately

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function onSubmitHandler(event){
-    //event.preventDefault();
-    let element = document.querySelector('#lead-date');
-    let hiddenDate = document.querySelector('.hidden-date');    
-    hiddenDate.value = new Date(element.value).toLocaleDateString('en-IN');
+function onSubmitHandler(event) {
+  //event.preventDefault();
+  let element = document.querySelector("#lead-date");
+  let hiddenDate = document.querySelector(".hidden-date");
+  hiddenDate.value = new Date(element.value).toLocaleDateString("en-IN");
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
